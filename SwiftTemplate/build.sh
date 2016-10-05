d9bfbce9de3147b646a88a077bd32a4a53839c15
Marc Lavergne <mlavergn@gmail.com>
2016-10-05 00:40:44 -0400

    Remove harding to sim from build.sh

--- SwiftTemplate/build.sh
+++ SwiftTemplate/build.sh
@@ -10,39 +10,66 @@
 
 TARGET_IOS="armv7-apple-ios10.0"
 TARGET_SIM="x86_64-apple-ios10.0"
-TARGET_MACOS="x86_64-apple-macosx10.10"
+TARGET_MAC="x86_64-apple-macosx10.10"
 
 SDK_IOS="iPhoneOS"
 SDK_SIM="iPhoneSimulator"
-SDK_MACOS="MacOSX"
-
-TARGET_OLD=$TARGET_MACOS
-SDK_OLD=$SDK_MACOS
-
-TARGET_NEW=$TARGET_SIM
-SDK_NEW=$SDK_SIM
+SDK_MAC="MacOSX"
 
 if [ ! -f .build/debug.yaml ]; then
   swift build --configuration debug
-  sed -i '' -- "s/$SDK_OLD.platform/$SDK_NEW.platform/g" .build/debug.yaml
-  sed -i '' -- "s/$SDK_OLD[0-9.]*.sdk/$SDK_NEW.sdk/g" .build/debug.yaml
-  sed -i '' -- "s/$TARGET_OLD/$TARGET_NEW/g" .build/debug.yaml
 fi
 
 if [ ! -f .build/release.yaml ]; then
   swift build --configuration release
-  sed -i '' -- "s/$SDK_OLD.platform/$SDK_NEW.platform/g" .build/release.yaml
-  sed -i '' -- "s/$SDK_OLD[0-9.]*.sdk/$SDK_NEW.sdk/g" .build/release.yaml
-  sed -i '' -- "s/$TARGET_OLD/$TARGET_NEW/g" .build/release.yaml
 fi
 
-if [ "$#" -ne 1 ]; then
-  echo "Requires parameter [debug / release / clean]"
+IS_IOS=`grep -c "$TARGET_IOS" .build/debug.yaml`
+IS_SIM=`grep -c "$TARGET_SIM" .build/debug.yaml`
+IS_MAC=`grep -c "$TARGET_MAC" .build/debug.yaml`
+
+if [ "$IS_IOS" -eq 1 ]; then
+  TARGET_OLD=$TARGET_IOS
+  SDK_OLD=$SDK_IOS
+  CONFIG_OLD="ios"
+elif [ "$IS_SIM" -eq 1 ]; then
+  TARGET_OLD=$TARGET_SIM
+  SDK_OLD=$SDK_SIM
+  CONFIG_OLD="sim"
+elif [ "$IS_MAC" -eq 1 ]; then
+  TARGET_OLD=$TARGET_MAC
+  SDK_OLD=$SDK_MAC
+  CONFIG_OLD="mac"
+fi
+
+function reconfig {
+  if [ "$1" == "ios" ]; then
+    TARGET_NEW=$TARGET_IOS
+    SDK_NEW=$SDK_IOS
+  elif [ "$1" == "sim" ]; then
+    TARGET_NEW=$TARGET_SIM
+    SDK_NEW=$SDK_SIM
+  elif [ "$1" == "mac" ]; then
+    TARGET_NEW=$TARGET_MAC
+    SDK_NEW=$SDK_MAC
+  fi
+
+  sed -i '' -- "s/$SDK_OLD.platform/$SDK_NEW.platform/g" .build/debug.yaml
+  sed -i '' -- "s/$SDK_OLD[0-9.]*.sdk/$SDK_NEW.sdk/g" .build/debug.yaml
+  sed -i '' -- "s/$TARGET_OLD/$TARGET_NEW/g" .build/debug.yaml
+}
+
+if [ "$#" -ne 2 ]; then
+  echo "Requires parameters [ ios / sim / mac ] and [debug / release / clean]"
 else
-  if [ "$1" == "clean" ]; then
+  if [ "$2" == "clean" ]; then
     rm -r .build
   else
-    /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/swift-build-tool -C .build -f $1.yaml
+    if [ "$1" != "$CONFIG_OLD" ]; then
+      reconfig $1
+    fi
+
+    /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/swift-build-tool -C .build -f $2.yaml
   fi
 fi
 
